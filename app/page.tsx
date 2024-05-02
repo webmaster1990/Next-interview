67b47f55b45e9d85e105dcb05af2b73c1cd13e11
kschmott <kschmott@umich.edu>
2024-05-02 13:14:10 -0400

    all ready

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 /*
 TASK 1:
 Fetch a list of the first 10 people in the Star Wars API and display their names in a list. docs: https://swapi.dev/documentation#people
+For the sake of this exercise, you do not use the built in pagination feature of the API.
 TASK 2:
 Add a + button next to each name that when clicked will fetch and display the person's homeworld. docs: https://swapi.dev/documentation#people
 TASK 3:
---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default Providers;
